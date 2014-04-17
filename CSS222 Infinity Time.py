bdb4b469f03811acab51ba4e6651946eb66c5edb
bob huff <bob@arioch.us>
2014-04-17 12:00:20 -0600

    test

---
+++ CSS222 Infinity Time.py
@@ -0,0 +1,38 @@
+# QUIZ
+# 
+# Complete the forward_euler() function for the 
+# differential equation and set the variable 
+# end_time to where the solution completely 
+# explodes! Give two decimal places of precision.
+
+import sys
+
+from udacityplots import *
+
+end_time = 42.
+h = 0.0001
+num_steps = int(end_time / h)
+times = h * numpy.array(range(num_steps + 1))
+
+x = numpy.zeros(num_steps + 1)
+
+x[0] = 3.
+
+def forward_euler():
+    for step in range(num_steps):
+        x[step+1] = x[step] + h* (x[step]**2+1)/2###Your code here.
+        print x[step], step, h * step
+        if x[step] >1e14:
+            exit(0)
+
+    #########check the content of x and of end_time
+    return x
+
+x = forward_euler()
+
+@show_plot
+def plot_me():
+    matplotlib.pyplot.plot(times, x)
+    matplotlib.pyplot.show()
+
+plot_me()
